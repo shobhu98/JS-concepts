32eb6ea8deedc18f0a527a2e62a5197832f801c1
shobhu98 <46121289+shobhu98@users.noreply.github.com>
2019-06-30 21:54:11 +0530

    a

--- classes.js
+++ classes.js
@@ -2,9 +2,17 @@ function Person(name,age) {
     this.firstName=name.split(" ")[0];
     this.lastname=name.split(" ")[1];
     this.age=age;
+     this.isAdult=function() {
+        return age>18;
+
+    }
 
 
 }
 
 p=new Person("John Snow",30);
 console.log(p);
+console.log(p.isAdult());
+// ask  here the doubt
+q=Person("Love First",34);
+console.log(q);
